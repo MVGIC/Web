5c2fd9d5f502fed9e4e9cf6b578b36dbf2bbb6e0
MVGIC <ex3ct@mail.ru>
2021-05-24 12:27:53 +0300

    updates#1

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -10,6 +10,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
+
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -51,14 +52,16 @@ class CallbackTest {
 //    }
 
     @Test
-    void shouldTestV1(){
+    void shouldTestV1() throws InterruptedException {
         driver.get("http://localhost:9999");
-        driver.findElement(By.className("input__top")).sendKeys("Иван Иванов");
-        driver.findElement(By.className("input__inner")).sendKeys("+78005553535");
-        driver.findElement(By.className("check__box")).click();
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Иван Иванов");
+        elements.get(1).sendKeys("+78005553535");
+        driver.findElement(By.className("checkbox__box")).click();
         driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.className("order-success")).getText();
-        assertEquals("Ваша заявка успешно отправлена!",text.trim());
+        String text = driver.findElement(By.className("Success_successBlock__2L3Cw")).getText();
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",text.trim());
+        Thread.sleep(50000);
     }
 
 //    @Test
