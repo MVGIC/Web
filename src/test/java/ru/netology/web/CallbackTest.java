b55b8c63f4cca66b97330c58edaaf1f1b56a7c1e
MVGIC <ex3ct@mail.ru>
2021-05-30 12:31:44 +0300

    build for the new Project

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -44,7 +44,7 @@ class CallbackTest {
         driver.findElement(By.cssSelector("button")).click();
         String text = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText();
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",text.trim());
-        Thread.sleep(5000);
+        Thread.sleep(5001);
     }
 }
 
