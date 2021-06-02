446fdc51bebc922c0e824e76e7ce426c55327858
MVGIC <ex3ct@mail.ru>
2021-06-03 01:24:08 +0300

    test #2

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -46,5 +46,16 @@ class CallbackTest {
         assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.",text.trim());
         Thread.sleep(5001);
     }
+
+    @Test
+    void shouldTestV2() throws InterruptedException {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Иван Иванов");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+78005553535");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        driver.findElement(By.className("input_invalid"));
+        Thread.sleep(5001);
+    }
 }
 
